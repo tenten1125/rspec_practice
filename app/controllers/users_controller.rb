3af3b1d9d411aa52d4e60788ceabaefef11994af
yoshito <yoshito5211@gmail.com>
2021-08-20 23:07:05 +0900

    indexのリクエストスペックを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,17 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 end
