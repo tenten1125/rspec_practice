69834f39101cb626eefb6b043af5dbd6aa39644b
yoshito <yoshito5211@gmail.com>
2021-08-25 22:51:11 +0900

    updateアクションのリクエストスペックを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update; end
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user
+    else
+      render :edit
+    end
+  end
 
   def destroy; end
 
